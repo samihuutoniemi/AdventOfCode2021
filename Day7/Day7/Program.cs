4205ac85d7661cc9a67e875ea2c6900f3365faeb
samihuutoniemi <sami.huutoniemi@gmail.com>
2021-12-07 19:19:29 +0100

    Day 7

---
+++ Day7/Day7/Program.cs
@@ -0,0 +1,45 @@
+﻿//var initialPositions = File.ReadAllText("input.txt")
+//    .Split(new[] { ',' }).Select(x => int.Parse(x)).ToArray();
+
+//var dict = Enumerable.Range(initialPositions.Min(), initialPositions.Max() - initialPositions.Min() + 1)
+//    .ToDictionary(x => x, x => 0);
+
+//foreach (var key in dict.Keys)
+//{
+//    dict[key] = GetFuelCostForPosition(initialPositions, key);
+//}
+
+//var min = dict.Min(x => x.Value); // Task 1
+
+//static int GetFuelCostForPosition(int[] positions, int target)
+//{
+//    return positions.Select(x => Math.Abs(x-target)).Sum();
+//}
+
+var initialPositions = File.ReadAllText("input.txt")
+    .Split(new[] { ',' }).Select(x => int.Parse(x)).ToArray();
+
+var dict = Enumerable.Range(initialPositions.Min(), initialPositions.Max() - initialPositions.Min() + 1)
+    .ToDictionary(x => x, x => 0);
+
+foreach (var key in dict.Keys)
+{
+    dict[key] = GetTotalFuelCostForAllCrabs(initialPositions, key);
+}
+
+var min = dict.Min(x => x.Value); // Result
+
+static int GetFuelCostForCrab(int position, int target)
+{
+    var distance = Math.Abs(target - position);
+    var cost = Enumerable.Range(1, distance).Sum();
+
+    return cost;
+}
+
+static int GetTotalFuelCostForAllCrabs(int[] positions, int target)
+{
+    return positions.Select(x => GetFuelCostForCrab(x, target)).Sum();
+}
+
+return;
